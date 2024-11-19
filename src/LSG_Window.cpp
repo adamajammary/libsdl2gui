3a64d8ec146b7105a56998177ef2967cd4bd584e
Adam A. Jammary <adam@jammary.com>
2024-11-19 14:05:53 +0100

    add: file-picker on android (#101)

--- src/LSG_Window.cpp
+++ src/LSG_Window.cpp
@@ -443,20 +443,22 @@ std::vector<std::string> LSG_Window::OpenFolders()
 #elif defined _android
 std::string LSG_Window::OpenFile()
 {
-	auto jniEnvironment = LSG_AndroidJNI::GetEnvironment();
-	auto jniActivity    = LSG_AndroidJNI::GetClass(LSG_ConstAndroid::ActivityClassPath, jniEnvironment);
-	auto jniOpenFile    = jniEnvironment->GetStaticMethodID(jniActivity, "OpenFile", "()V");
+	auto jniEnvironment   = LSG_AndroidJNI::GetEnvironment();
+	auto jniActivity      = LSG_AndroidJNI::GetClass(LSG_ConstAndroid::ActivityClassPath, jniEnvironment);
+	auto jniIsOpeningFile = jniEnvironment->GetStaticFieldID(jniActivity,  "IsOpeningFile", "Z");
+	auto jniOpenedFile    = jniEnvironment->GetStaticFieldID(jniActivity,  "OpenedFile",    "java/lang/String");
+	auto jniOpenFile      = jniEnvironment->GetStaticMethodID(jniActivity, "OpenFile",      "()V");
 
 	jniEnvironment->CallStaticVoidMethod(jniActivity, jniOpenFile);
 
-	while (jniEnvironment->GetStaticBooleanField(jniActivity, "IsOpeningFile"))
+	while (jniEnvironment->GetStaticBooleanField(jniActivity, jniIsOpeningFile))
 		SDL_Delay(10);
 
-	auto jniOpenedFile = (jstring)jniEnvironment->GetStaticObjectField(jniActivity, "OpenedFile");
-	auto jniFileUTF8   = jniEnvironment->GetStringUTFChars(jniOpenedFile, NULL);
-	auto selectedFile  = std::string(jniFileUTF8);
+	auto openedFile   = (jstring)jniEnvironment->GetStaticObjectField(jniActivity, jniOpenedFile);
+	auto fileUTF8     = jniEnvironment->GetStringUTFChars(openedFile, NULL);
+	auto selectedFile = std::string(fileUTF8);
 
-	jniEnvironment->ReleaseStringUTFChars(jniOpenedFile, jniFileUTF8);
+	jniEnvironment->ReleaseStringUTFChars(openedFile, fileUTF8);
 	jniEnvironment->DeleteLocalRef(jniActivity);
 
 	return selectedFile;
