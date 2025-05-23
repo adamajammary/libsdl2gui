20f10e8c2ea94e41da9245ae9afe96c110277cbd
Adam A. Jammary <adam@jammary.com>
2025-05-23 18:04:07 +0200

    fix(build): undefined reference to hb_ft_face_create on linux (#143)

--- src/LSG_Window.cpp
+++ src/LSG_Window.cpp
@@ -196,9 +196,9 @@ LSG_Strings LSG_Window::openFiles(bool openFolder, bool allowMultipleSelection, 
 		(openFolder ? "Select a folder" : "Select a file"),
 		nullptr,
 		(openFolder ? GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER : GTK_FILE_CHOOSER_ACTION_OPEN),
-		GTK_STOCK_CANCEL,
+		_("_Cancel"),
 		GTK_RESPONSE_CANCEL,
-		GTK_STOCK_OPEN,
+		_("_Open"),
 		GTK_RESPONSE_ACCEPT,
 		nullptr
 	);
@@ -735,9 +735,9 @@ std::string LSG_Window::SaveFile(const LSG_Strings& filters)
 		"Save File",
 		nullptr,
 		GTK_FILE_CHOOSER_ACTION_SAVE,
-		GTK_STOCK_CANCEL,
+		_("_Cancel"),
 		GTK_RESPONSE_CANCEL,
-		GTK_STOCK_SAVE_AS,
+		_("_Save"),
 		GTK_RESPONSE_ACCEPT,
 		nullptr
 	);
