7e7a45b3eb65f48de16ec5e1c7d10c126beb623f
Adam A. Jammary <adam@jammary.com>
2026-08-23 15:39:17 +0200

    fix(sdl3): dpi scaling (#244)

--- src/LSG_Window.cpp
+++ src/LSG_Window.cpp
@@ -905,15 +905,14 @@ void LSG_Window::SetDPIScale()
 {
 	// TODO: DPI Android?
 
-	auto ds = SDL_GetWindowDisplayScale(LSG_Window::window);
-	auto pd = SDL_GetWindowPixelDensity(LSG_Window::window); // windows
-	auto cs = SDL_GetDisplayContentScale(SDL_GetWindowID(LSG_Window::window));
+	//auto ds = SDL_GetWindowDisplayScale(LSG_Window::window);
+	//auto pd = SDL_GetWindowPixelDensity(LSG_Window::window);
+	//auto cs = SDL_GetDisplayContentScale(SDL_GetWindowID(LSG_Window::window));
 
-	LSG_Window::dpiScale = LSG_Window::GetSizeScale().x; // windows
+	LSG_Window::dpiScale = LSG_Window::GetSizeScale().x;
 
-	auto m = std::format("DS {:.2f} | PD {:.2f} | CS {:.2f} | DPI {:.2f}", ds, pd, cs, LSG_Window::dpiScale);
-
-	SDL_ShowSimpleMessageBox(0, "DPI", m.c_str(), LSG_Window::window);
+	//auto m = std::format("DS {:.2f} | PD {:.2f} | CS {:.2f} | DPI {:.2f}", ds, pd, cs, LSG_Window::dpiScale);
+	//SDL_ShowSimpleMessageBox(0, "DPI", m.c_str(), LSG_Window::window);
 }
 
 void LSG_Window::SetMaximized(bool maximized)
